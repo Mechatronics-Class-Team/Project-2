dfe141197358192457a72f739e04f7fe30a18586
ggosset2017 <71349400+ggosset2017@users.noreply.github.com>
2020-09-27 14:00:40 -0400

    added atmel stuff

---
+++ Atmel Studio/Project_2/Project_2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Project_2.c
+ *
+ * Created: 9/27/2020 13:59:46
+ * Author : Gage Gosset
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
